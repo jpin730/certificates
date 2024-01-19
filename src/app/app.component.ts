4d33cbb687023cbf846e3c9e1bf357fbbf89d06f
Jaime Pineda <jpin730@pm.me>
2024-01-19 14:06:07 -0600

    chore: :wrench: configure eslint and prettier with husky hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    
-  `,
+  template: ` <h1>Welcome to {{ title }}!</h1> `,
   styles: [],
 })
 export class AppComponent {
-  title = 'certificates';
+  title = 'certificates'
 }
