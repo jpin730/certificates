c0926c0264566de92bbaa6c14776480725d1ae83
Jaime Pineda <jpin730@pm.me>
2024-01-19 16:34:31 -0600

    feat: :lipstick: add main layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common'
 import { Observable, take } from 'rxjs'
 
 import { AppService, Certificate } from './app.service'
+import { FooterComponent } from './components/footer/footer.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
   private readonly appService = inject(AppService)
 
   certificates$!: Observable<Certificate[]>
+  showAlert = true
 
   ngOnInit(): void {
     this.certificates$ = this.appService.getCertificates().pipe(take(1))
