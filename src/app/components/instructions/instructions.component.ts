726da58ee725e11eee5d773d50a3e8cb45755e09
Jaime Pineda <jpin730@pm.me>
2024-01-19 20:24:35 -0600

    refactor: :lipstick: refactor instructions component as a pop-up message

--- src/app/components/instructions/instructions.component.ts
+++ src/app/components/instructions/instructions.component.ts
@@ -1,11 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-instructions',
   standalone: true,
   templateUrl: './instructions.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InstructionsComponent {
+export class InstructionsComponent implements OnInit {
   showAlert = true
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.showAlert = false
+    }, 4000)
+  }
 }
